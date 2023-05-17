bc2490ac090e2e45a1963dc8a31474e06016d246
Titus <contact@titusl.com>
2023-05-17 02:50:40 -0700

    added dynamic about page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,29 +2,31 @@ import { defineConfig } from 'astro/config';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
-
+import tailwind from "@astrojs/tailwind";
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        storyblok({
-            accessToken: env.STORYBLOK_TOKEN,
-            components: {
-                blogpost: 'storyblok/BlogPost',
-                blogpostlist: 'storyblok/BlogPostList',
-                page: 'storyblok/Page',
-            },
-            apiOptions: {
-                // Choose your Storyblok space region
-                region: 'us', // optional,  or 'eu' (default)
-            },
-        })
-  ],
-    vite: {
-        plugins: [basicSsl()],
-        server: {
-        https: true,
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      blogpost: 'storyblok/BlogPost',
+      blogpostlist: 'storyblok/BlogPostList',
+      page: 'storyblok/Page',
+      employeelist: 'storyblok/Employee',
     },
-  },
+    apiOptions: {
+      // Choose your Storyblok space region
+      region: 'us' // optional,  or 'eu' (default)
+    }
+  }), tailwind({
+    config: { applyBaseStyles: false },
+  })],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true
+    }
+  }
 });
